50de1ac8378bca4bc5d83c000d921f63b9581dac
spooonsss <spooonsss123@gmail.com>
2021-11-16 21:33:40 -0600

    If offscreen, don't show smoke sprite or play sound effect

--- sprite_killer_box.asm
+++ sprite_killer_box.asm
@@ -52,8 +52,11 @@ endif
 		LDX #!SprSize
 	---
 		DEX
-		BMI +++
-		
+		BPL +
+		LDX $15E9|!Base2
+		RTS
+		+
+
 		CPX $15E9|!Base2
 		BEQ ---
 		
@@ -83,36 +86,66 @@ else
 		
 		JSL $03B6E5
 endif
-		LDA $02
+		LDA $02 ; $02 - Clipping width
 		LSR
 		CLC
-		ADC $00
-		STA $0C		
-		
-		LDA $03
+		ADC $00 ; $00 - Clipping X displacement low
+		STA $00
+
+		LDA #$00
+		ADC $08 ; $08 - Clipping X displacement, high
+		XBA
+		LDA $00
+
+		REP #$20
+		SEC
+		SBC.w #$0008
+		PHA
+		SEC
+		SBC $1A
+		CMP.w #$0100
+		PLA
+		SEP #$20
+		BCS .offscreen
+		STA $0C
+
+		LDA $03 ; $03 - Clipping height
 		LSR
 		CLC
-		ADC $01
-		STA $0E
+		ADC $01 ; $01 - Clipping Y displacement low
+		STA $01
+
+		LDA #$00
+		ADC $09 ; $09 - Clipping Y displacement, high
+		XBA
+		LDA $01
+
+		REP #$20
+		SEC
+		SBC.w #$0008
+		PHA
+		SEC
+		SBC $1C
+		CMP.w #$0100
+		PLA
+		SEP #$20
+		BCS .offscreen
 		
 		LDY $1863|!Base2	; Get smoke sprite index.
+
+		STA $17C4|!Base2,y	; Y position
 		
 		LDA #$01
 		STA $17C0|!Base2,y	; Smoke sprite type (smoke)
 		LDA $0C
-		SEC
-		SBC #$08
 		STA $17C8|!Base2,y	; Smoke X position
-		LDA $0E
-		SEC
-		SBC #$08
-		STA $17C4|!Base2,y	; Y position
 		LDA #20
 		STA $17CC|!Base2,y	; Timer
 		
 		LDA #$25
 		STA $1DFC|!Base2	; Sound effect (Yoshi stomps)
-		
+
+.offscreen
 		STZ !14C8,x
 		
 +++
